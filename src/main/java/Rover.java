37a338a77c50fb13b9307bd4bea9335cd868164e
Trotyl Yu <yzj1995@vip.qq.com>
2015-12-31 13:43:19 +0800

    [Trotyl] Update Rover to store position rather than bare x and y.

--- src/main/java/Rover.java
+++ src/main/java/Rover.java
@@ -1,16 +1,13 @@
 public class Rover {
-    private int x;
-    private int y;
+    private Position position;
     private Orientation orientation;
 
     public Rover(int x, int y, Orientation orientation, Range range) {
-        this.x = x;
-        this.y = y;
+        this.position = new Position(x, y);
         this.orientation = orientation;
     }
 
     public Status execute(String commands) {
-        Position position = new Position(0, 0);
         if (commands.equals("L")) {
             return new Status(position, Orientation.W);
         }
